12a7b195852a784cb4c02d87da2909f7ccaf9689
MMargherita <margherita.moretti@uniroma1.it>
2024-03-21 13:07:31 +0200

    small changes

--- code/04_decomp_periods.R
+++ code/04_decomp_periods.R
@@ -3,33 +3,33 @@
 # load shared functions
 source("code/00_functions.R")
 
-# load Umats for 07, 15, 16, 19
-# first period decomp for 2007 - 2009
-load("data/data_out/Umats_07.rda")
-Umat_f_07 <- matrices$female
-Umat_m_07 <- matrices$male
-
-load("data/data_out/Umats_09.rda")
-Umat_f_09 <- matrices$female
-Umat_m_09 <- matrices$male
-
-# second period decomp for 2011 - 2015
-load("data/data_out/Umats_11.rda")
-Umat_f_11 <- matrices$female
-Umat_m_11 <- matrices$male
-
-load("data/data_out/Umats_15.rda")
-Umat_f_15 <- matrices$female
-Umat_m_15 <- matrices$male
-
-# third period decomp for 2016 - 2019
-load("data/data_out/Umats_16.rda")
-Umat_f_16 <- matrices$female
-Umat_m_16 <- matrices$male
-
-load("data/data_out/Umats_19.rda")
-Umat_f_19 <- matrices$female
-Umat_m_19 <- matrices$male
+# # load Umats for 07, 15, 16, 19
+# # first period decomp for 2007 - 2009
+# load("data/data_out/Umats_07.rda")
+# Umat_f_07 <- matrices$female
+# Umat_m_07 <- matrices$male
+# 
+# load("data/data_out/Umats_09.rda")
+# Umat_f_09 <- matrices$female
+# Umat_m_09 <- matrices$male
+# 
+# # second period decomp for 2011 - 2015
+# load("data/data_out/Umats_11.rda")
+# Umat_f_11 <- matrices$female
+# Umat_m_11 <- matrices$male
+# 
+# load("data/data_out/Umats_15.rda")
+# Umat_f_15 <- matrices$female
+# Umat_m_15 <- matrices$male
+# 
+# # third period decomp for 2016 - 2019
+# load("data/data_out/Umats_16.rda")
+# Umat_f_16 <- matrices$female
+# Umat_m_16 <- matrices$male
+# 
+# load("data/data_out/Umats_19.rda")
+# Umat_f_19 <- matrices$female
+# Umat_m_19 <- matrices$male
 
 period_left  <- c("07","11","16")
 period_right <- c("09","15","19")
@@ -39,11 +39,11 @@ for (s in c("m", "f")) {
     sex <- ifelse(sex == "m", "male", "female")
     
     Umat_left_name  <- paste0("Umats_", period_left[p], ".rda")
-    Umat_left       <- local(get(load(file.path("data/data_out/data_out",Umat_left_name))))
+    Umat_left       <- local(get(load(file.path("data/data_out/",Umat_left_name))))
     Umat_left       <- Umat_left[[sex]]
     
     Umat_right_name <- paste0("Umats_", period_right[p], ".rda")
-    Umat_right      <- local(get(load(file.path("data/data_out/data_out",Umat_right_name))))
+    Umat_right      <- local(get(load(file.path("data/data_out/",Umat_right_name))))
     Umat_right      <- Umat_right[[sex]]
     
     P_left <- from_U_to_trans_matrix(Umat_left) |>
@@ -82,7 +82,7 @@ for (s in c("m", "f")) {
 }
 
 # decompose again using H:
-for (sex in c("m", "f")) {
+for (s in c("m", "f")) {
   for (p in 1:3) {
     sex <- ifelse(s == "m", "male", "female")
     
